770c1ec6902d25c5c5caed072bb4b2f14affc62e
Loachlan <loahclan96@gmail.com>
2019-10-07 10:43:46 +0200

    VectorGenerator Class Added

---
+++ Thesis_HJC885/VectorGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thesis_HJC885
+{
+    public static class VectorGenerator
+    {
+        static Random r = new Random();
+
+        static Vector3 Generate_Random_Vector3(int from, int to)
+        {
+            return new Vector3((float)r.Next(from, to), (float)r.Next(from, to), (float)r.Next(from, to));
+        }
+
+    }
+}
