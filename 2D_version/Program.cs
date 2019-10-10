424a5a644da5149060b1910e1cc3dc2c99e37395
Loachlan <loahclan96@gmail.com>
2019-10-10 15:16:38 +0200

    2D_version evolved to Main

--- 2D_version/Program.cs
+++ 2D_version/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,18 @@ namespace _2D_version
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("Random Generált Vektorok koordinátái: \n");
+
+            IEnumerable<Vector2> vs = VectorGenerator.Generate_Multiple_Random_Vector3(600, -45, 45);
+
+
+
+            ToConsole.Vectors_To_Console(vs);
+
+            Console.ReadKey();
+
         }
     }
 }
---
+++ 2D_version/ToConsole.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2D_version
+{
+    public static class ToConsole
+    {
+
+
+        public static void Vectors_To_Console(IEnumerable<Vector2> vectors)
+        {
+            foreach (Vector2 item in vectors)
+            {
+                Console.WriteLine(item.ToString() + "\n");
+                //Console.WriteLine("X: {0} \nY: {1} \nZ: {2}\n\n", item.X, item.Y, item.Z);
+            }
+
+        }
+
+    }
+}
