e4852429f7fecbb9814633d8dbc47d3cfe08c8c6
Loachlan <loahclan96@gmail.com>
2019-10-10 15:22:17 +0200

    Config class Added

---
+++ 2D_version/Config.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2D_version
+{
+    static class Config
+    {
+        static readonly double DistanceRobotFromBuetStart = 200;
+        static readonly double[] Map_size = new double[] { 200, 200 };
+        static readonly double Bullet_Speed = 2;
+        static readonly double Bullet_Size = 6;
+
+        static readonly double[] Robot_Start_Location = new double[] { 50, 50 };
+
+    }
+}
