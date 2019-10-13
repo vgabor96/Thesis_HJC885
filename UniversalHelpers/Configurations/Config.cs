dbcf684169dbdd87d3b9b81741983f5721f850b0
Gábor Vár <loahclan96@gmail.com>
2019-10-13 20:46:39 +0200

    Robot const ID = 1 Added

--- UniversalHelpers/Configurations/Config.cs
+++ UniversalHelpers/Configurations/Config.cs
@@ -9,17 +9,17 @@ namespace UniversalHelpers.Configurations
 
     public static class Config
     {
-    
+        //MAP
         public const double DistanceRobotFromBuetStart = 200;
         public const int Default_Map_size_X = 800;
         public const int Default_Map_size_Y = 600;
 
-
+        //VECTORS
         public const int Default_Vector_Count = 1;
         public const double Default_Vector_from = -6;
         public const double Default_Vector_to = 6;
 
-
+        //BULLET
         public const int Default_Bullet_Count = 1;
 
         public const int Default_Bullet_Location_x=150;
@@ -33,7 +33,8 @@ namespace UniversalHelpers.Configurations
         public const double Max_Bullet_Size = 10;
         public const double Min_Bullet_Size = 1;
 
-
+        // ROBOT
+        public const int ID = 1;
         public const int Robot_Start_Location_X = 20;
         public const int Robot_Start_Location_Y = 20;
         public const double Robot_range = 1;
