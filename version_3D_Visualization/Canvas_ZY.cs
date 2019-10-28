c7b3e7797c55b63c7ba5de3454236670d6105914
Gábor Vár <loahclan96@gmail.com>
2019-10-28 21:28:27 +0100

    Canvas_ XZ Added

---
+++ version_3D_Visualization/Canvas_ZY.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace version_3D_Visualization
+{
+    class Canvas_ZY
+    {
+    }
+}
