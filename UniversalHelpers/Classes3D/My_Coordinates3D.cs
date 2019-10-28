47019cb1b93399c45e65eb62c476f50faf1342a6
Gábor Vár <loahclan96@gmail.com>
2019-10-28 18:19:09 +0100

    Classes3D Added

---
+++ UniversalHelpers/Classes3D/My_Coordinates3D.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+using UniversalHelpers.Configurations;
+
+namespace UniversalHelpers.Classes3D
+{
+   public class My_Coordinates3D
+    {
+       
+            private int x;
+            private int y;
+
+            public int X { get => this.x; set => this.x = value; }
+            public int Y { get => this.y; set => this.y = value; }
+
+            public My_Coordinates3D(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            //public void GenerateRandomCoordinate()
+            //{
+
+            //    this.x = RandomGenerator.r.Next(0, Config.Default_Map_size_X);
+            //    this.y = RandomGenerator.r.Next(0, Config.Default_Map_size_Y);
+            //}
+
+            public static Vector3 GenerateRandomVector2()
+            {
+                float v1 = 0;
+                float v2 = 0;
+                float v3 = 0;
+
+                v1 = (float)RandomGenerator3D.r.Next(Convert.ToInt32(Config.Default_Vector_from), Convert.ToInt32(Config.Default_Vector_to));
+
+                //Vectors should moving => both of them cannot be 0.
+                while (v1 == 0)
+                {
+                    v1 = (float)RandomGenerator3D.r.Next(Convert.ToInt32(Config.Default_Vector_from), Convert.ToInt32(Config.Default_Vector_to));
+
+                }
+                v2 = (float)RandomGenerator3D.r.Next(Convert.ToInt32(Config.Default_Vector_from), Convert.ToInt32(Config.Default_Vector_to));
+
+                v3 = (float)RandomGenerator3D.r.Next(Convert.ToInt32(Config.Default_Vector_from), Convert.ToInt32(Config.Default_Vector_to));
+
+            return new Vector3(v1, v2, v3);
+            }
+
+            public static Line LineFromTwoPoints(My_Coordinates3D first, My_Coordinates3D second)
+            {
+                Line line = new Line()
+                {
+                    X1 = first.X,
+                    X2 = second.X,
+                    Y1 = first.Y,
+                    Y2 = second.Y
+                };
+                return line;
+
+            }
+        //    public static bool DoesLineContainPoint(Line line, My_Coordinates3D point, double linewidth = 0)
+        //    {
+
+
+        //        if (linewidth == 0)
+        //        {
+        //            bool first = point.X >= Math.Min(line.X1, line.X2);
+        //            bool sec = point.X <= Math.Max(line.X1, line.X2);
+        //            bool third = point.Y >= Math.Min(line.Y1, line.Y2);
+        //            bool fou = point.Y <= Math.Max(line.Y1, line.Y2);
+
+        //            if (point.X >= Math.Min(line.X1, line.X2) &&
+        //                point.X <= Math.Max(line.X1, line.X2) &&
+        //                point.Y >= Math.Min(line.Y1, line.Y2) &&
+        //                point.Y <= Math.Max(line.Y1, line.Y2))
+        //            {
+        //                return true;
+        //            }
+        //        }
+        //        else
+        //        {
+        //            double width = linewidth / 2;
+        //            if (point.X >= Math.Min(line.X1, line.X2) && point.X <= Math.Max(line.X1, line.X2) && point.Y >= Math.Min(line.Y1, line.Y2) && point.Y <= Math.Max(line.Y1, line.Y2))
+        //            {
+        //                return true;
+        //            }
+
+        //        }
+        //        return false;
+        //    }
+    }
+
+}
---
+++ UniversalHelpers/Classes3D/RandomGenerator3D.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using UniversalHelpers.Configurations;
+
+namespace UniversalHelpers.Classes3D
+{
+    public static class RandomGenerator3D
+    { 
+
+            public static readonly Random r = new Random();
+
+            public static IEnumerable<Vector3> Generate_Multiple_Random_Vector2(int count = Config.Default_Vector_Count, double from = Config.Default_Vector_from, double to = Config.Default_Vector_to)
+            {
+                float v1 = 0;
+                float v2 = 0;
+                float v3 = 0;
+                for (int i = 0; i < count; i++)
+                {
+
+
+
+                    v1 = (float)r.Next(Convert.ToInt32(from), Convert.ToInt32(to));
+
+
+
+                    v2 = (float)r.Next(Convert.ToInt32(from), Convert.ToInt32(to));
+
+                    v3 = (float)r.Next(Convert.ToInt32(from), Convert.ToInt32(to));
+
+                    yield return new Vector3(v1, v2,v3);
+                }
+
+            }
+        }
+}
