101d1eea01297152d1a2262ed4c1e3c2a96175ea
vgabor96 <loahclan96@gmail.com>
2019-11-26 00:08:24 +0100

    lenajpg to float* -> float* to lena jpg

---
+++ ImageProcessing/Opencv_Test/first.cpp
@@ -0,0 +1,39 @@
+#include<opencv2/opencv.hpp>
+#include<iostream>
+using namespace std;
+using namespace cv;
+int main()
+{
+	//load the image
+	Mat img = imread("lena.jpg");
+	namedWindow("oldlena", WINDOW_NORMAL);
+	imshow("oldlena", img);
+
+	//read height, width data
+	int height = img.rows;
+	int width = img.cols;
+
+
+	//check whether image loaded is empty or not.
+	if (img.empty())
+	{
+		cerr << "no image"; return -1;
+	}
+
+	//convert image from CV::MAT to float*.
+	Mat dst;
+	img.convertTo(dst, CV_32F);
+	float* data = dst.ptr<float>();
+
+	//convert back the image from float* to CV::MAT.
+	Mat dest(height, width, CV_32FC3, data);
+
+	//print the image
+	imwrite("newlena.jpg", dest);
+	Mat img2 = imread("newlena.jpg");
+	namedWindow("newlena", WINDOW_NORMAL);
+	imshow("newlena", img2);
+
+	waitKey(0);
+	return 0;
+}
