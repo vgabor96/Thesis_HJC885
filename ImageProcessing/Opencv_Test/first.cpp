a48e7ea3baf8a14b9afd0807f54f53b2af6f7240
vgabor96 <loahclan96@gmail.com>
2019-11-26 10:57:58 +0100

    2 pictures added

--- ImageProcessing/Opencv_Test/first.cpp
+++ ImageProcessing/Opencv_Test/first.cpp
@@ -2,14 +2,24 @@
 #include<iostream>
 using namespace std;
 using namespace cv;
+
 int main()
 {
 	//load the image
-	Mat img = imread("lena.jpg");
-	namedWindow("oldlena", WINDOW_NORMAL);
-	imshow("oldlena", img);
+	//Mat img = imread("lena.jpg");
+	Mat img = imread("C:\\Users\\loahc\\Documents\\GitHub\\Thesis_HJC885\\Unity\\Thesis_HJC885\\Assets\\screenshots\\screen_1024x768_2019-11-25_15-22-04_01.png");
+	
+	namedWindow("pic_1", WINDOW_NORMAL);
+	resizeWindow("pic_1", img.cols, img.rows);
+	imshow("pic_1", img);
+
+	Mat img2 = imread("C:\\Users\\loahc\\Documents\\GitHub\\Thesis_HJC885\\Unity\\Thesis_HJC885\\Assets\\screenshots\\screen_1024x768_2019-11-25_15-22-04_02.png");
+	namedWindow("newpic", WINDOW_NORMAL);
+	resizeWindow("newpic", img2.cols, img2.rows);
+	imshow("newpic", img2);
 
 	//read height, width data
+	
 	int height = img.rows;
 	int width = img.cols;
 
@@ -25,8 +35,12 @@ int main()
 	img.convertTo(dst, CV_32F);
 	float* data = dst.ptr<float>();
 
+	Mat dst2;
+	img2.convertTo(dst2, CV_32F);
+	float* data2 = dst2.ptr<float>();
+
 	//Grayscale
-	for (int i = 0; i < (height*width)*3; i+=3)
+	/*for (int i = 0; i < (height*width)*3; i+=3)
 	{
 		float r = data[i];
 		float g = data[i + 1];
@@ -37,17 +51,49 @@ int main()
 		data[i + 1] = avg;
 		data[i + 2] = avg;
 	}
+	*/
+
+	//sub
+	/*
+	for (int i = 0; i < (height * width) * 3; i += 3)
+	{
+		float r = abs((data[i] - data2[i]) / 2);
+		float g = abs((data[i + 1] - data2[i + 1]) / 2);
+		float b = abs((data[i + 2] - data2[i + 2]) / 2);
+
+		//float avg = (r + g + b) / 3;
+		data[i] = r;
+		data[i + 1] = g;
+		data[i + 2] = b;
+	}
+	*/
+	//Add
 	
+	for (int i = 0; i < (height * width) * 3; i += 3)
+	{
+		float r = (data[i]+data2[i])/2;
+		float g = (data[i+1] + data2[i+1]) / 2;
+		float b = (data[i+2] + data2[i+2]) / 2;
+
+		float avg = (r + g + b) / 3;
+		data[i] = r;
+		data[i + 1] = g;
+		data[i + 2] = b;
+	}
+	
+
+
 
 	//convert back the image from float* to CV::MAT.
 	Mat dest(height, width, CV_32FC3, data);
 
 	//print the image
-	imwrite("newlena.jpg", dest);
-	Mat img2 = imread("newlena.jpg");
-	namedWindow("newlena", WINDOW_NORMAL);
-	imshow("newlena", img2);
-
+	imwrite("addedpic.jpg", dest);
+	Mat img3 = imread("addedpic.jpg");
+	namedWindow("addedpic", WINDOW_NORMAL);
+	resizeWindow("addedpic", img3.cols, img3.rows);
+	imshow("addedpic", img3);
+	
 	waitKey(0);
 	return 0;
 }
