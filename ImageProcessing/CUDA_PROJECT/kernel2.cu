ff24b13fb8427eb191864b0d127f294ddb794429
vgabor96 <loahclan96@gmail.com>
2019-12-09 23:01:47 +0100

    hough circles adjusted

--- ImageProcessing/CUDA_PROJECT/kernel2.cu
+++ ImageProcessing/CUDA_PROJECT/kernel2.cu
@@ -2,6 +2,8 @@
 #include "device_launch_parameters.h"
 #include <math.h>  
 #include <io.h>
+#include <iostream>
+#include <fstream>
 #include <stdio.h>
 #include <stdlib.h> 
 #include <time.h> 
@@ -30,30 +32,26 @@ int main()
 	int nothitcounter = 0;
 	printf("Picture Size N: %d \n", N);
 	printf("Picture Size M: %d \n", M);
-	string falsstring = "False";
-	for (size_t i = 0; i < filenames.size()-1; i+=2)
+	int falsstring = 0;
+	ofstream myfile;
+	myfile.open("results.txt");
+	
+	
+	for (size_t x = 0; x < filenames.size()-1; x+=2)
 	{
-		Mat img = imread(filenames[i]);
-		Mat img2 = imread(filenames[i+1]);
+		Mat img = imread(filenames[x]);
+		Mat img2 = imread(filenames[x+1]);
 
 		
-		if (filenames[i].find("True") != std::string::npos) {
-			falsstring = "True";
-			hitcounter++;
-		}
-		else
-		{
-			falsstring = "False";
-			nothitcounter++;
-		}
+		////SHOW IMAGE
 
-		namedWindow("pic_1", WINDOW_NORMAL);
-		resizeWindow("pic_1", img.cols, img.rows);
-		imshow("pic_1", img);
+		//namedWindow("pic_1", WINDOW_NORMAL);
+		//resizeWindow("pic_1", img.cols, img.rows);
+		//imshow("pic_1", img);
 
-		namedWindow("pic_2", WINDOW_NORMAL);
-		resizeWindow("pic_2", img2.cols, img2.rows);
-		imshow("pic_2", img2);
+		//namedWindow("pic_2", WINDOW_NORMAL);
+		//resizeWindow("pic_2", img2.cols, img2.rows);
+		//imshow("pic_2", img2);
 
 
 
@@ -90,9 +88,11 @@ int main()
 		//print the image
 		imwrite("subpic.jpg", dest);
 		Mat img3 = imread("subpic.jpg");
-		namedWindow("subpic", WINDOW_NORMAL);
+
+		//SHOW IAMGE
+	/*	namedWindow("subpic", WINDOW_NORMAL);
 		resizeWindow("subpic", img3.cols, img3.rows);
-		imshow("subpic", img3);
+		imshow("subpic", img3);*/
 
 
 		//Makewhite and black only
@@ -118,9 +118,10 @@ int main()
 		//print the image
 		imwrite("pic1_black.jpg", dest3);
 		Mat img4 = imread("pic1_black.jpg");
-		namedWindow("pic1_black", WINDOW_NORMAL);
+		//SHOW IMAGE
+	/*	namedWindow("pic1_black", WINDOW_NORMAL);
 		resizeWindow("pic1_black", img4.cols, img4.rows);
-		imshow("pic1_black", img4);
+		imshow("pic1_black", img4);*/
 
 		Mat src, src_gray;
 
@@ -134,22 +135,23 @@ int main()
 
 		/// Convert it to gray
 		cvtColor(src, src_gray, COLOR_RGB2GRAY);
-		namedWindow("Hough Circle Transform Gray", WINDOW_NORMAL);
-		imshow("Hough Circle Transform Gray", src);
+		////SHOW IMAGE
+		//namedWindow("Hough Circle Transform Gray", WINDOW_NORMAL);
+		//imshow("Hough Circle Transform Gray", src);
 
 		/// Reduce the noise so we avoid false circle detection
 		GaussianBlur(src_gray, src_gray, Size(9, 9), 2, 2);
-		namedWindow("Hough Circle Transform Gauss", WINDOW_NORMAL);
-		imshow("Hough Circle Transform Gauss", src);
+		////SHOW IMAGE
+		//namedWindow("Hough Circle Transform Gauss", WINDOW_NORMAL);
+		//imshow("Hough Circle Transform Gauss", src);
 
 		vector<Vec3f> circles;
-		for (int maxR = 10; maxR < 200; maxR = maxR + 200 - 9)
-		{
+		
 
 			/// Apply the Hough Transform to find the circles
 			//		WorkingHoughCircles(src_gray, circles, HOUGH_GRADIENT, 1, src_gray.rows / 100, 10, 10, 0, 100);
 			//HoughCircles(src_gray, circles, HOUGH_GRADIENT, 1,0.5, 10, 7, 0, 10);
-			HoughCircles(src_gray, circles, HOUGH_GRADIENT, 1,0.5, 10, 7, 0, 10);
+			HoughCircles(src_gray, circles, HOUGH_GRADIENT, 1,5, 10, 7, 1, 10);
 
 			/// Draw the circles detected
 			for (size_t i = 0; i < circles.size(); i++)
@@ -160,36 +162,47 @@ int main()
 				circle(src, center, 2, Scalar(0, 255, 0), 2, 8, 0);
 				// circle outline
 				circle(src, center, radius, Scalar(0, 0, 255), 2, 8, 0);
-				if (i > 0)
+				if (i > 0 && i< 2)
 				{
+					if (filenames[x].find("True") != std::string::npos) {
+						falsstring = 1;
+						hitcounter++;
+					}
+					else
+					{
+						falsstring = 0;
+						nothitcounter++;
+					}
+
 					Point center1(cvRound(circles[i - 1][0]), cvRound(circles[i - 1][1]));
 					printf("%i,", center1.x);
 					printf("%i,", center1.y);
+				
 
 					Point center2(cvRound(circles[i][0]), cvRound(circles[i][1]));
 
 					printf("%i,", center2.x);
 					printf("%i,", center2.y);
-					printf("%s\n",falsstring.c_str());
-
+					printf("%i\n",falsstring);
+					myfile << center1.x << "," << center1.y <<","<< center2.x << "," << center2.y<<","<<falsstring<<"\n";
 					line(src, center1, center2, Scalar(0, 0, 255), 2, 8, 0);
 				}
 			}
-		}
+		
 
 
 		/// Show your results
 		namedWindow("Hough Circle Transform", WINDOW_NORMAL);
 		imshow("Hough Circle Transform", src);
 
+		//WAIT OR NOT
 		waitKey(0);
 	}
 
 
-	/*Mat img = imread("C:\\Users\\loahc\\Documents\\GitHub\\Thesis_HJC885\\Unity\\Thesis_HJC885\\Assets\\screenshots\\testpic\\screen_1024x768_2019-11-25_15-22-04_01.png");
-	Mat img2 = imread("C:\\Users\\loahc\\Documents\\GitHub\\Thesis_HJC885\\Unity\\Thesis_HJC885\\Assets\\screenshots\\testpic\\screen_1024x768_2019-11-25_15-22-04_02.png");
-	*/
+
 	printf("Number of Hits: %i\n", hitcounter);
 	printf("Number of Non-Hits: %i\n", nothitcounter);
+	myfile.close();
 	return 0;
 }
